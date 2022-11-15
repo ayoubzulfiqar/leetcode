c0b812d2a94046307430934985eed0ab2d83097f
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2022-11-15 11:18:09 -0800

    leetcode

--- RangeSumQuery-Immutable/range_sum_query_immutable.go
+++ RangeSumQuery-Immutable/range_sum_query_immutable.go
@@ -1,1 +1,17 @@
 package main
+
+type NumArray struct {
+	sums []int
+}
+
+func Constructor(nums []int) NumArray {
+	sums := make([]int, len(nums)+1)
+	for i, v := range nums {
+		sums[i+1] += v + sums[i]
+	}
+	return NumArray{sums}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	return this.sums[right+1] - this.sums[left]
+}
