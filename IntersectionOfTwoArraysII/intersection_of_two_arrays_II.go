3607b189ff1a59cca7dd6855d9ba569341462e36
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2022-11-17 11:48:15 -0800

    leetcode

--- IntersectionOfTwoArraysII/intersection_of_two_arrays_II.go
+++ IntersectionOfTwoArraysII/intersection_of_two_arrays_II.go
@@ -1,1 +1,23 @@
 package main
+
+import "sort"
+
+func intersect(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	var result []int
+	var left int = 0
+	var right int = 0
+	for left < len(nums1) && right < len(nums2) {
+		if nums1[left] == nums2[right] {
+			result = append(result, nums1[left])
+			left++
+			right++
+		} else if nums1[left] < nums2[right] {
+			left++
+		} else {
+			right++
+		}
+	}
+	return result
+}
