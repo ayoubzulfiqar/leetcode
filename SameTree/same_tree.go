dd22a41e93e49472ede0a92a451ce1fbec61a20e
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2023-01-10 10:39:44 +0000

    leetcode

--- SameTree/same_tree.go
+++ SameTree/same_tree.go
@@ -16,8 +16,3 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	}
 	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
-
-/*
-Runtime: 0 ms, faster than 100.00% of Go online submissions for Same Tree.
-Memory Usage: 2.1 MB, less than 67.42% of Go online submissions for Same Tree.
-*/
