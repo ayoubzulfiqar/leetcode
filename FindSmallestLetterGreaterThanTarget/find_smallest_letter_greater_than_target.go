39d22470c744e369fefa760125ccd957feecf5fd
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2023-06-09 03:22:24 -0700

    leetcode

--- FindSmallestLetterGreaterThanTarget/find_smallest_letter_greater_than_target.go
+++ FindSmallestLetterGreaterThanTarget/find_smallest_letter_greater_than_target.go
@@ -1,1 +1,18 @@
 package main
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	left := 0
+	right := len(letters) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if letters[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return letters[left%len(letters)]
+}
