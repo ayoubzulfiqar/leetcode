1476f9b705e28cb57a346079d95a79781acdf43e
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2023-09-01 12:40:44 -0700

    leetcode

--- CountingBits/counting_bits.go
+++ CountingBits/counting_bits.go
@@ -1,16 +1,24 @@
 package main
 
+// func countBits(n int) []int {
+// 	arr := make([]int, n+1)
+// 	arr[0] = 0
+// 	prevPow := 1
+// 	for i := 1; i <= n; i++ {
+// 		if i&(i-1) == 0 {
+// 			arr[i] = 1
+// 			prevPow = i
+// 			continue
+// 		}
+// 		arr[i] = 1 + arr[i-prevPow]
+// 	}
+// 	return arr
+// }
+
 func countBits(n int) []int {
-	arr := make([]int, n+1)
-	arr[0] = 0
-	prevPow := 1
+	var array []int = make([]int, n+1)
 	for i := 1; i <= n; i++ {
-		if i&(i-1) == 0 {
-			arr[i] = 1
-			prevPow = i
-			continue
-		}
-		arr[i] = 1 + arr[i-prevPow]
+		array[i] = array[i>>1] + (i & 1)
 	}
-	return arr
+	return array
 }
